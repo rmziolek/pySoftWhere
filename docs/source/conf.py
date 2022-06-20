1366706952eee7c8a610fe5573dbc15503f245eb
Rob Ziolek <rmz@Robs-iMac.lan>
2022-06-20 15:52:53 +0100

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'pySoftWhere'
+copyright = '2022, R.M. Ziolek'
+author = 'Rob Ziolek'
 
 release = '0.1'
 version = '0.1.0'
